37875c772d92d4d43b72d7ac502d943e0df00a9d
WolfY_D3v <larueulrick89@gmail.com>
2025-12-21 11:30:46 +0100

    Re-factored the base level generation system

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -19,12 +19,12 @@ func _ready() -> void:
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_LEFT):
-		if player_line_idx - 1 > 0:
-			player.position.x -= 1.0
+		if player_line_idx - 1 >= 0:
+			_set_player_position(player_line_idx - 1)
 			player_line_idx -= 1
 	if Input.is_key_pressed(KEY_RIGHT):
-		if player_line_idx + 1 <= nb_lines:
-			player.position.x += 1.0
+		if player_line_idx + 1 < nb_lines:
+			_set_player_position(player_line_idx + 1)
 			player_line_idx += 1
 	if Input.is_key_pressed(KEY_DOWN):
 		player.position.y = 0.5
@@ -40,9 +40,16 @@ func build_level(_level_file_path: String) -> void:
 	var _level_data: Dictionary = LevelManager.load_level()
 	level_data = _level_data
 	
+	await _generate_lines()
+	player_line_idx = int(nb_lines / 2)
+	_set_player_position(player_line_idx - 1)
+
+func _set_player_position(line_idx: int) -> void:
+	player.position.x = lines.get_child(line_idx).position.x
+
+func _generate_lines() -> void:
 	# Fetch the number of lines of the level from the loaded level data
 	nb_lines = int(level_data.get("lines"))
-	print(nb_lines)
 	
 	# Generate the lines
 	for line_idx: int in range(nb_lines):
@@ -51,8 +58,8 @@ func build_level(_level_file_path: String) -> void:
 		_line.position = line_position
 		
 		line_position.x += _line.mesh.size.x
-	
-	# Set the player postion
-	player_line_idx = int(nb_lines / 2)
-	player.position.x = lines.get_child(player_line_idx - 1).position.x
-	print(player_line_idx)
+		
+		player_line_idx = int(line_idx / 2)
+		_set_player_position(player_line_idx)
+		
+		await get_tree().create_timer(0.1).timeout
