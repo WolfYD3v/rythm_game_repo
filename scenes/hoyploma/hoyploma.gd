0d4f3fd8ab4ceae4ab431053450c5e8a3487163e
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-04-06 22:27:01 +0200

    OMG this title screen is amazing damn...

--- scenes/hoyploma/hoyploma.gd
+++ scenes/hoyploma/hoyploma.gd
@@ -1,31 +1,25 @@
 extends Node3D
 class_name Hoyploma
 
-@onready var camera: Camera3D = $Camera
+@onready var top_camera: Camera3D = $TopCamera
+@onready var camera_pivot: Node3D = $CameraPivot
+@onready var camera: Camera3D = $CameraPivot/Camera
 @onready var building: Node3D = $Building
 
-@export_range(0.1, 0.2, 0.1, "or_greater", "hide_control") var camera_rotation_speed: float = 5.0
+@export var top_camera_view: bool = false
+@export_range(0.05, 0.2, 0.05) var camera_rotation_speed: float = 0.1
 
 const NEW_TSCN_RESOURCE_PATH: String = "user://hoyploma.tscn"
 
 func _ready() -> void:
+	top_camera.current = top_camera_view
 	SceneManager.replacing_scene.connect(pack_scene)
-	
-	var temp_camera_tween = get_tree().create_tween()
-	temp_camera_tween.tween_property(
-		camera, "global_position",
-		camera.global_position + Vector3(
-			15,
-			-8,
-			22
-		),
-		10.0
-	)
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	camera_pivot.rotate_y(camera_rotation_speed * delta)
 	camera.look_at_from_position(
 		camera.global_position,
-		building.global_position,
+		building.global_position
 	)
 
 func pack_scene() -> void:
