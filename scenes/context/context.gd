5380c187c66864f242248caf8fada2a1ff9635be
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-04-12 11:00:22 +0200

    Context Scene Added

--- scenes/context/context.gd
+++ scenes/context/context.gd
@@ -1,21 +1,33 @@
 extends Control
 class_name Context
 
+@onready var animation_player: AnimationPlayer = $SubViewport/AnimationPlayer
 @onready var text_label: Label = $TextLabel
+@onready var _3dbg_texture_rect: TextureRect = $_3DBGTextureRect
+
+@export var force_play: bool = false
 
 const SONG_SELECTION_PACKED_SCENE: PackedScene = preload("res://scenes/song_selection/song_selection.tscn")
 
-var writing_duration: float = 15.0
+var writing_duration: float = 0.0
 
 func _ready() -> void:
+	var move_char_animation: Animation = animation_player.get_animation("move_char")
+	writing_duration = move_char_animation.length
+	
+	_3dbg_texture_rect.modulate = Color(1.0, 1.0, 1.0, 0.0)
 	text_label.visible_ratio = 0.0
-	if not SaveManager.get_save_data("context_seen"):
+	if not SaveManager.get_save_data("context_seen") or force_play:
 		await get_tree().create_timer(1.0).timeout
+		animate_3d_bg_texture_rect()
+		
 		await write_text()
 		print("Writing Finished !")
 		SaveManager.save_game(
 			{"context_seen": true}
 		)
+		
+		await get_tree().create_timer(5.0).timeout
 	
 	SceneManager.add_scene("SongSelection", SONG_SELECTION_PACKED_SCENE)
 	SceneManager.replace_scene("SongSelection")
@@ -27,3 +39,12 @@ func write_text() -> void:
 		1.0, writing_duration
 	)
 	await text_label_tween.finished
+
+func animate_3d_bg_texture_rect():
+	animation_player.play("move_char")
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(
+		_3dbg_texture_rect, "modulate",
+		Color(1.0, 1.0, 1.0, 1.0), writing_duration
+	)
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -4,12 +4,16 @@ class_name MainMenu
 @onready var hoyploma_sub_viewport: SubViewport = $HoyplomaSubViewport
 @onready var credits_rich_text_label: RichTextLabel = $CreditsRichTextLabel
 @onready var gui: Control = $GUI
+@onready var hoyploma_bg: TextureRect = $HoyplomaBG
 
 const SETTINGS_PACKED_SCENE: PackedScene = preload("res://scenes/settings/settings.tscn")
 const CONTEXT_PACKED_SCENE: PackedScene = preload("res://scenes/context/context.tscn")
 const HOYPLOMA_PACKED_SCENE: PackedScene = preload("res://scenes/hoyploma/hoyploma.tscn")
 
 func _ready() -> void:
+	gui.show()
+	hoyploma_bg.show()
+	
 	set_credits_visibility(false)
 	SceneManager.add_scene("Hoyploma", HOYPLOMA_PACKED_SCENE)
 	hoyploma_sub_viewport.add_child(
@@ -28,6 +32,14 @@ func _on_settings_button_pressed() -> void:
 	SceneManager.replace_scene("Settings")
 
 func _on_play_button_pressed() -> void:
+	gui.hide()
+	hoyploma_bg.hide()
+	await get_tree().create_timer(1.5).timeout
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property($ColorRect, "modulate", Color(0.0, 0.0, 0.0, 0.0), 5.0)
+	await tween.finished
+	
 	SceneManager.add_scene("Context", CONTEXT_PACKED_SCENE)
 	SceneManager.replace_scene("Context")
 
@@ -40,5 +52,6 @@ func _on_credits_button_pressed() -> void:
 		
 		credits_rich_text_label.text = "TEMP !!!!!!\n%s" % credits
 		set_credits_visibility(true)
+
 func _on_close_credits_button_pressed() -> void:
 	set_credits_visibility(false)
